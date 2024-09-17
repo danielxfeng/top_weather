a103a49faa7f1d71a0d6eaf4e65adac83efc186a
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-09-17 23:30:11 +0300

    Updated static part, and some controller part.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./style.css";
 
+let form = document.getElementById("search_form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let city= e.target.elements["city"].value;
+  console.log(city);
+});
+
+
+
+
+
